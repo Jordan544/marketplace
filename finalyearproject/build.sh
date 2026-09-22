2e212d45207462225d89455d0947bbdca1efd71e
Jordan544 <mainactor2024@gmail.com>
2026-09-22 14:54:32 +0300

    update2

--- finalyearproject/build.sh
+++ finalyearproject/build.sh
@@ -1,3 +1,4 @@
 set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'yoursecurepassword')"
